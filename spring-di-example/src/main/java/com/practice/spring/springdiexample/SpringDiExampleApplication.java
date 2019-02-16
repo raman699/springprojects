63737c9f3f716e4778c77c3804d7c95eac5a18ae
raman699 <raman.699@rediff.com>
2019-02-16 13:22:56 +0530

    added new project diexample

---
+++ spring-di-example/src/main/java/com/practice/spring/springdiexample/SpringDiExampleApplication.java
@@ -0,0 +1,14 @@
+package com.practice.spring.springdiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiExampleApplication.class, args);
+	}
+
+}
+
---
+++ spring-di-example/src/test/java/com/practice/spring/springdiexample/SpringDiExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.practice.spring.springdiexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDiExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
